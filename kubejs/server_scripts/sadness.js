cc5e6ca9d60f1e1c5fe7494680508d272709c083
a1qs <ankiller2007.09.20@gmail.com>
2025-07-14 19:12:56 +0200

    fix: vdiamond block -> 9x vdiamond conversion

--- kubejs/server_scripts/sadness.js
+++ kubejs/server_scripts/sadness.js
@@ -1,3 +1,17 @@
 onEvent("recipes", event => {
     event.remove({ mod: 'refinedstorage'}) // bye RS recipes
+
+
+    event.shaped(Item.of('the_vault:vault_diamond_block'),
+            [
+                'GXG',
+                'XRX',
+                'GXG'
+            ], {
+            G: 'the_vault:vault_essence_2',
+            X: 'the_vault:vault_diamond_block_1',
+            R: 'the_vault:pog_prism'
+        })
+
+    event.shapeless(Item.of('the_vault:vault_diamond', 9), ['the_vault:vault_diamond_block'])
 })
