5ab7c1bc318eb32a3b361887282bb29d9da2a31d
a1qs <ankiller2007.09.20@gmail.com>
2025-08-02 23:04:38 +0200

    fix: duplicate alchemy seal recipe

--- kubejs/server_scripts/sadness.js
+++ kubejs/server_scripts/sadness.js
@@ -1,19 +1,3 @@
 onEvent("recipes", event => {
     event.remove({ mod: 'refinedstorage'}) // bye RS recipes
-
-   // alchemy seal
-    event.shaped(
-      Item.of('woldsvaults:crystal_seal_alchemy', 1),
-      [
-        'DXD',
-        'DCD',
-        'DGD'
-      ],
-      {
-        D: 'the_vault:vault_diamond',
-        X: 'the_vault:vault_essence',
-        C: 'the_vault:crystal_seal_empty',
-        G: 'minecraft:glass_bottle',
-      }
-    )
 })
